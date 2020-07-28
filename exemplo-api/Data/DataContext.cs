8b1f3780e467b1112f5c518b9f7e599d87a75852
VanderNunesBR <tec.vander.nunes@gmail.com>
2020-07-28 20:10:36 -0300

    Adicionado persistência em Mysql.

--- exemplo-api/Data/DataContext.cs
+++ exemplo-api/Data/DataContext.cs
@@ -9,13 +9,13 @@ namespace exemplo_api.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-
-        }
-
-        public DbSet<Product> Products { get; set; }
+         public DbSet<Product> Products { get; set; }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseMySql("server=127.0.0.1;userid=root;password=456852;database=SGM_Cidadao"));
+        }
     }
 }
